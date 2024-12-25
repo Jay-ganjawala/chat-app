288b6c23cc2f9eac5ac4723ab3e97a7ab36a1e6a
Jay <90839627+Jay-ganjawala@users.noreply.github.com>
2024-12-25 17:31:58 +0000

    Commit

--- server.js
+++ server.js
@@ -94,4 +94,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 300
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
