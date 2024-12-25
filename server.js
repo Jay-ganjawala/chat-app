448292913a499b85d50b607f004493ad1d59b6dd
Jay <90839627+Jay-ganjawala@users.noreply.github.com>
2024-12-25 17:07:32 +0000

    Commit

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-const users = {}; // In-memory user storage (for demo purposes)
+const users = {}; // In-memory user storage
+const onlineUsers = {}; // Track online users
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -35,6 +36,7 @@ app.post('/login', (req, res) => {
     const { username } = req.body;
     if (users[username]) {
         req.session.username = username;
+        onlineUsers[username] = true; // Mark user as online
         res.redirect('/profile');
     } else {
         res.send('User  not found. Please sign up.');
@@ -50,6 +52,7 @@ app.post('/signup', (req, res) => {
     if (!users[username]) {
         users[username] = true; // Store user
         req.session.username = username;
+        onlineUsers[username] = true; // Mark user as online
         res.redirect('/profile');
     } else {
         res.send('User  already exists. Please log in.');
@@ -58,13 +61,14 @@ app.post('/signup', (req, res) => {
 
 app.get('/profile', (req, res) => {
     if (req.session.username) {
-        res.render('profile', { username: req.session.username });
+        res.render('profile', { username: req.session.username, onlineUsers: Object.keys(onlineUsers) });
     } else {
         res.redirect('/login');
     }
 });
 
 app.get('/logout', (req, res) => {
+    onlineUsers[req.session.username] = false; // Mark user as offline
     req.session.destroy();
     res.redirect('/login');
 });
@@ -73,16 +77,21 @@ app.get('/logout', (req, res) => {
 io.on('connection', (socket) => {
     console.log('A user connected');
 
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+    socket.on('join', (username) => {
+        socket.username = username; // Store username in socket
+        socket.join(username); // Join a room with the username
+        io.emit('user list', Object.keys(onlineUsers)); // Update user list
+    });
+
+    socket.on('chat message', (data) => {
+        io.to(data.to).emit('chat message', { from: data.from, message: data.message });
     });
 
     socket.on('disconnect', () => {
         console.log('User  disconnected');
+        delete onlineUsers[socket.username]; // Remove user from online users
+        io.emit('user list', Object.keys(onlineUsers)); // Update user list
     });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 300
